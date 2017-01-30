6ab0a08ebfac55858d4f3c95acfaf0835cf75888
mahhov1 <mahhov1@gmail.com>
2017-01-29 19:54:08 -0800

    text colors

--- pythonTwitch.py
+++ pythonTwitch.py
@@ -1,22 +1,31 @@
-#!python3
-
 import sys
 import subprocess
-
 try :
     from twitch.api import v3
 except:
     print("please sudo pip3 install python-twitch")
 
+class bcolors:
+    HEADER = '\033[1m\033[93m'
+    LINE1 = '\033[1m\033[97m'
+    LINE2 = '\033[1m\033[0m'
+    ENDC = '\033[0m'
+
+
 sums = []
 def printChannels(query = "dota2"):
-    print(" -- page {} --".format(page + 1))
+    print(bcolors.HEADER + " -- page {} --".format(page + 1))
+    print("-"*50)
     streams = v3.search.streams('dota2', 15, page * 15)['streams']
     for i, stream in enumerate(streams):
         sum = [i + 1, stream["channel"]["display_name"], stream["viewers"], stream["channel"]["url"]]
         sums.append(sum)
-        print("{}   :   {}  ({})".format(sum[0], sum[1], sum[2]))
-        #print("      {}".format(sum[3]))
+        if (i % 2 == 0):
+            print(bcolors.LINE1, end="")
+        else:
+            print(bcolors.LINE2, end="")
+        print("{:4}:   {:20}  ({})".format(sum[0], sum[1], sum[2]))
+    print(bcolors.ENDC)
     sys.stdout.flush()
 
 def gotoChannel(selection):
