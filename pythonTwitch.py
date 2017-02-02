64b6bbd03c98f5456e02aee2fa8e4b1fca0fac16
mahhov1 <mahhov1@gmail.com>
2017-02-02 08:47:50 -0800

    major changes for yt support

--- pythonTwitch.py
+++ pythonTwitch.py
@@ -48,15 +48,17 @@ def gotoSum(selection):
     selSum = sums[selection]
     print('going to {}'.format(selSum[1]))
     if (twitch):
+        vid = selSum[3]
         if (mac):
-            execute = "streamlink -np 'quicktime player' '{}' best".format(selSum[3]) 
+            execute = "streamlink -np 'quicktime player' '{}' best".format(vid) 
         else:
-            execute = "streamlink -np 'omxplayer' '{}' best".format(selSum[3]) 
+            execute = "streamlink -np 'omxplayer' '{}' best".format(vid) 
     else:
+        vid = pafy.new(selSum[3]).getbest().url
         if (mac):
-            execute = "open -a 'quicktime player' '{}'".format(selSum[3])
+            execute = "open -a 'quicktime player' '{}'".format(vid)
         else:
-            execute = "omxplayer '{}'".format(selSum[3])
+            execute = "omxplayer '{}'".format(vid)
     if (debug):
         print("execute : {}".format(execute))
     else:
@@ -67,28 +69,35 @@ def gotoSum(selection):
             print(ValueError)
 
 def getTwitchChannels():
+    sums = []
     streams = v3.search.streams(query, 15, page * 15)['streams']
     for i, stream in enumerate(streams):
-        sum = [i + 1, stream["channel"]["display_name"], stream["viewers"], stream["channel"]["url"]]
-        sums.append(sum)
+        title = stream["channel"]["display_name"]
+        viewers = stream["viewers"]
+        streamUrl = stream["channel"]["url"]
+        sums.append([i + 1, title, viewers, streamUrl])
 
 def getYoutubeChannels():
-    resp = urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key="+key+"&q="+query)
-    respRead = resp.read()
-    respObj = json.loads(respRead)
-    for i, item in enumerate(respObj['items']):
+    sums = []
+    search = json.loads(urlopen("https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key="+key+"&q="+query).read())
+    for i, item in enumerate(search['items']):
+        print(item)
         code = item['id']['videoId']
-        video = pafy.new(code)
-        sum = [i + 1, video.title, video.duration, video.getbest().url]
-        sums.append(sum)
+        title = item['snippet']['title']
+        details = json.loads(urlopen("https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key="+key+"&id="+code).read())
+        duration = details['items'][0]['contentDetails']['duration']
+        # video = pafy.new(code, basic = False) # video.getbest().url]
+        sums.append([i + 1, title, duration, code])
 
 def makeQuery():
+    sums = []
     if (twitch):
         getTwitchChannels()
     else:
         getYoutubeChannels()
 
 def main():
+    global twitch, page, query
     makeQuery()
     displaySums()
     while(usrInupt == ""):
@@ -98,12 +107,12 @@ def main():
         elif (usrInput == "q"):
             exit()
         elif (usrInput == "t"):
-            twitch == True
+            twitch = True
             page = 0
             query = usrInput[1:]
             makeQuery()
         elif (usrInput == "y"):
-            twitch == False
+            twitch = False
             page = 0
             query = usrInput[1:]
             makeQuery()
